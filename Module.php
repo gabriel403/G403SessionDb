3697f3cfd698fd0d55d9d8995cf34717f9a2ef76
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2013-02-12 22:36:46 +0000

    tweaks

--- Module.php
+++ Module.php
@@ -68,10 +68,10 @@ class Module
         );
     }
 
-    // public function getConfig()
-    // {
-    //     return include __DIR__ . '/config/module.config.php';
-    // }
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
     public function getAutoloaderConfig()
     {
